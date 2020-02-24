eecb58fa3ff94f61aa0fcc8b919be18f8c50c13f
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 22:20:38 +0530

    create contact class and field email,lastname,contact,name

--- src/contactMenu/Contact.java
+++ src/contactMenu/Contact.java
@@ -1,5 +1,11 @@
 package contactMenu;
 
-public class Contact {
+import java.util.LinkedList;
 
+public class Contact {
+    private LinkedList<Integer> mobileNo;
+    private String name;
+    private String lastName;
+    private Contact contact;
+    private String email;
 }
