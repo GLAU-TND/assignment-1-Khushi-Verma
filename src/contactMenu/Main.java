ac9afb2f9305ad1cd4d83d04b36e622aaf83be6d
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 22:28:41 +0530

    create Scanner class object for input

--- src/contactMenu/Main.java
+++ src/contactMenu/Main.java
@@ -1,7 +1,9 @@
 package contactMenu;
 
+import java.util.Scanner;
+
 public class Main extends ContactMenuMethod {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);      //scanner object for scanning inputs
     }
 }
