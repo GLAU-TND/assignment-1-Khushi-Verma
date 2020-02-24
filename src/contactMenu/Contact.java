09fc68f1c0e3f21a5048975f94f500bb9e2c6426
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 22:23:30 +0530

    create getter setter in contact class

--- src/contactMenu/Contact.java
+++ src/contactMenu/Contact.java
@@ -16,6 +16,45 @@ public class Contact implements Comparable<Contact> {
         this.mobileNo = mobileNo;
 
     }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LinkedList<Integer> getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(LinkedList<Integer> mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 
     @Override
     public int compareTo(Contact o) {
